0c539674e800f211aab43dfcee48b77d50a01f1d
Simone Anthony <simone.anthony1@yahoo.com>
2022-02-24 22:14:24 -0500

    states initialized, html table place holder added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [queryStr, setQueryStr] = useState('');
+  const [apiRes, setApiRes] = useState([]);
+  const [errMsg, setErrMsg] = useState('');
+
+  useEffect(() => {
+
+    (async () => {
+      try {
+        // do api call here
+      } catch (err) {
+        setErrMsg('An error occurred while loading the API call! Please try again later!')
+      }
+  })();
+
+  }, [queryStr]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <th>Key</th>
+          <th>Headline</th>
+          <th>Sub-Headline</th>
+          <th>Date</th>
+        </tr>
+        <tr>
+          <td>Test 1</td>
+          <td>Test 2</td>
+          <td>Test 3</td>
+        </tr>
+      </table>
     </div>
   );
 }

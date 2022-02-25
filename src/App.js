6756ef08f4fa7ce056a9982a87d86484c52e0a74
Simone Anthony <simone.anthony1@yahoo.com>
2022-02-24 22:35:33 -0500

    axios installed, table data is now mapped

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [queryStr, setQueryStr] = useState('');
-  const [apiRes, setApiRes] = useState([]);
-  const [errMsg, setErrMsg] = useState('');
+  const [queryStr, setQueryStr] = useState("");
+  const [events, setEvents] = useState([]);
+  const [errMsg, setErrMsg] = useState("");
 
   useEffect(() => {
-
     (async () => {
       try {
-        // do api call here
+        const {data: { items }} = await axios.get(
+          `https://challenge.nfhsnetwork.com/v2/search/events/upcoming?state_association_key=18bad24aaa&card=true&size=50&start=0`
+        );
+        setEvents(items);
       } catch (err) {
-        setErrMsg('An error occurred while loading the API call! Please try again later!')
+        setErrMsg(
+          "An error occurred while loading the API call! Please try again later!"
+        );
       }
-  })();
-
+    })();
   }, [queryStr]);
+
+  // const appendQueryStr = (query) => {
+  //   setQueryStr(query);
+  // }
+  const displayTableData = events.map(
+    ({ key, headline, subheadline, date }) => (
+      <tr key={key}>
+        <td>{key}</td>
+        <td>{headline}</td>
+        <td>{subheadline}</td>
+        <td>{date}</td>
+      </tr>
+    )
+  );
 
   return (
     <div className="App">
       <table>
-        <tr>
-          <th>Key</th>
-          <th>Headline</th>
-          <th>Sub-Headline</th>
-          <th>Date</th>
-        </tr>
-        <tr>
-          <td>Test 1</td>
-          <td>Test 2</td>
-          <td>Test 3</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Key</th>
+            <th>Headline</th>
+            <th>Sub-Headline</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>{displayTableData}</tbody>
       </table>
     </div>
   );

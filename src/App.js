f6d9e66d834bf01470dd664167af56615802ba5f
Simone Anthony <simone.anthony1@yahoo.com>
2022-02-24 23:12:15 -0500

    select element added and state added

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ function App() {
   const [queryStr, setQueryStr] = useState("");
   const [events, setEvents] = useState([]);
   const [errMsg, setErrMsg] = useState("");
+  const [associationKey, setAssociationKey] = useState("18bad24aaa");
 
   useEffect(() => {
     (async () => {
       try {
-        const {data: { items }} = await axios.get(
-          `https://challenge.nfhsnetwork.com/v2/search/events/upcoming?state_association_key=18bad24aaa&card=true&size=50&start=0`
+        const {
+          data: { items },
+        } = await axios.get(
+          `https://challenge.nfhsnetwork.com/v2/search/events/upcoming?state_association_key=${associationKey}&card=true&size=50&start=0${queryStr}`
         );
         setEvents(items);
       } catch (err) {
@@ -20,7 +23,7 @@ function App() {
         );
       }
     })();
-  }, [queryStr]);
+  }, [associationKey, queryStr]);
 
   // const appendQueryStr = (query) => {
   //   setQueryStr(query);
@@ -38,16 +41,23 @@ function App() {
 
   return (
     <div className="App">
-      <table>
-        <thead>
-          <tr>
+      <section className="filter-container">
+        <label htmlFor="association-key">Choose a Association Key:</label>
+        <select name="association-key" id="association-key" onChange={e => setAssociationKey(e.target.value)}>
+          <option value="18bad24aaa">GHSA</option>
+          <option value="542bc38f95">UIL</option>
+        </select>
+      </section>
+      <table className="table">
+        <thead className="table-head">
+          <tr className="table-row">
             <th>Key</th>
             <th>Headline</th>
             <th>Sub-Headline</th>
             <th>Date</th>
           </tr>
         </thead>
-        <tbody>{displayTableData}</tbody>
+        <tbody className="table-body">{displayTableData}</tbody>
       </table>
     </div>
   );
